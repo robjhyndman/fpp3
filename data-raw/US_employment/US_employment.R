9a3938b9b38f856023cfed154ba4dcd8847a2700
Rob J Hyndman <robjhyndman@gmail.com>
2019-08-07 12:26:52 +1000

    snake_case names and removed redundant grouping

--- data-raw/US_employment/US_employment.R
+++ data-raw/US_employment/US_employment.R
@@ -1,15 +1,13 @@
 library(tidyverse)
 library(readxl)
 library(tsibble)
-library(fpp3)
 
 # US employment data
 
-US_employment <- read_excel("data-raw/US_employment/US_employment.xls", sheet = "Monthly") %>%
+us_employment <- read_excel("data-raw/US_employment/US_employment.xls", sheet = "Monthly") %>%
   rename(Month = "DATE") %>%
   mutate(Month = yearmonth(Month)) %>%
   gather("Series_ID", employment, -Month)  %>%
-  group_by(Series_ID) %>%
   as_tsibble(key = Series_ID, index=Month)
 
-usethis::use_data(US_employment, overwrite = TRUE)
+usethis::use_data(us_employment, overwrite = TRUE)
