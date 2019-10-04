c84d6dd55c16751bc0116a78bfcd0f4e261ea414
Unknown <george.athanasopoulos@monash.edu>
2019-10-04 15:33:41 +0300

    US data updated.

--- data-raw/uschange.R
+++ data-raw/uschange.R
@@ -10,3 +10,24 @@ us_change <- readr::read_csv("data-raw/uschange.csv") %>%
   as_tsibble(index = Quarter)
 
 usethis::use_data(us_change, overwrite = TRUE)
+
+# US change NEW
+unrate <- readxl::read_xls("data-raw/US_change.xls", sheet = "Monthly") %>%
+  mutate(Month = yearmonth(DATE)) %>%
+  select(Month, everything(), -DATE) %>%
+  as_tsibble(index = Month)
+
+# need to aggregate to monthly by taking the last month of the quarter observation.
+# then we take the differences of these, i.e., change on the rate
+
+other <- readxl::read_xls("data-raw/US_change.xls", sheet = "Quarterly") %>%
+  mutate(Quarter = yearquarter(DATE)) %>%
+  select(Quarter, everything(), -DATE) %>%
+  as_tsibble(index = Quarter) %>%
+  filter_index("1969 Q4"~.)
+
+# need to take differences of the logs * 100 to have growth rates of these.
+
+# We then merge the two tsibbles.
+
+
