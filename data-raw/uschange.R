56b73d2c1ac42e38e39652c7c2639742de0ab6f1
Rob J Hyndman <robjhyndman@gmail.com>
2019-10-05 18:44:36 +1000

    Updated uschange.R

--- data-raw/uschange.R
+++ data-raw/uschange.R
@@ -2,38 +2,54 @@ library(tidyverse)
 library(readxl)
 library(tsibble)
 
-# US change
+# US change data
+# Downloaded from https://research.stlouisfed.org/pdl/1216/
 
-us_change <- readr::read_csv("data-raw/uschange.csv") %>%
+# Read monthly unemployment data
+unrate <- readxl::read_xls("data-raw/uschange_fpp3.xls", sheet = "Monthly") %>%
+  mutate(Month = yearmonth(observation_date)) %>%
+  select(Month, everything(), -observation_date) %>%
+  rename(Unemployment = UNRATE_20191004) %>%
+  as_tsibble(index = Month)
+
+# Select last month of each quarter
+unrate <- unrate %>%
+  mutate(Quarter = yearquarter(Month)) %>%
+  index_by(Quarter) %>%
+  summarise(Unemployment = tail(Unemployment, 1)) %>%
+  filter_index("1969 Q4" ~ .)
+
+# Read remaining quarterly data
+other <- readxl::read_xls("data-raw/uschange_fpp3.xls", sheet = "Quarterly") %>%
+  mutate(Quarter = yearquarter(observation_date)) %>%
+  select(Quarter, everything(), -observation_date) %>%
+  rename(
+    Consumption = DPIC96_20191004,
+    Income = IPB50001SQ_20191004,
+    Production = PCECC96_20191004,
+    Savings = PSAVE_20191004
+  ) %>%
+  as_tsibble(index = Quarter) %>%
+  filter_index("1969 Q4" ~ .)
+
+# Joint the two tsibbles and compute percentage changes
+# need to take differences of the logs * 100 to have growth rates of these.
+us_change <- inner_join(other, unrate) %>%
+  mutate(
+    Consumption = difference(log(Consumption)) * 100,
+    Income = difference(log(Income)) * 100,
+    Production = difference(log(Production)) * 100,
+    Savings = difference(log(Savings)) * 100,
+    Unemployment = difference(Unemployment)
+  ) %>%
+  filter_index("1970 Q1" ~ .)
+
+us_change
+
+# Compare with csv file
+readr::read_csv("data-raw/uschange.csv") %>%
   mutate(Quarter = yearquarter(Time)) %>%
   select(Quarter, everything(), -Time) %>%
   as_tsibble(index = Quarter)
 
-usethis::use_data(us_change, overwrite = TRUE)
-
-# US change NEW
-unrate <- readxl::read_xls("data-raw/uschange_fpp3.xls", sheet = "Monthly") %>%
-  mutate(Month = yearmonth(observation_date)) %>%
-  select(Month, everything(), -observation_date) %>%
-  rename(Unemployment=UNRATE_20191004) %>%
-  as_tsibble(index = Month)
-
-# need to aggregate to monthly by taking the last month of the quarter as the observation.
-# then we take the differences of these, i.e., calculate the change of the rate
-# then select 1970 onwards
-
-other <- readxl::read_xls("data-raw/uschange_fpp3.xls", sheet = "Quarterly") %>%
-  mutate(Quarter = yearquarter(observation_date)) %>%
-  select(Quarter, everything(), -observation_date) %>%
-  rename(Consumption=DPIC96_20191004,
-         Income= IPB50001SQ_20191004,
-         Production=PCECC96_20191004,
-         Savings=PSAVE_20191004) %>%
-  as_tsibble(index = Quarter) %>%
-  filter_index("1969 Q4"~.)
-
-# need to take differences of the logs * 100 to have growth rates of these.
-
-# We then merge the two tsibbles.
-
-
+# usethis::use_data(us_change, overwrite = TRUE)
