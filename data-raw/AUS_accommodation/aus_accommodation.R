af7ec79211970cdc819c37a5e1f11a99553f7645
Rob Hyndman <robjhyndman@gmail.com>
2020-12-19 13:57:38 +1100

    Consistent capitalization and details of units for aus_accommodation

--- data-raw/AUS_accommodation/aus_accommodation.R
+++ data-raw/AUS_accommodation/aus_accommodation.R
@@ -5,28 +5,28 @@ library(readabs)
 aus_accommodation <- read_abs_local(filenames="8635010.xlsx", path="data-raw/AUS_accommodation/") %>%
   filter(series_type == "Original") %>%
   mutate(
-    date = yearquarter(date),
-    state = str_extract(series,";  [A-Za-z\\s\\(\\)]* ;"),
-    state = str_trim(str_remove_all(state, ";")),
+    Date = yearquarter(date),
+    State = str_extract(series,";  [A-Za-z\\s\\(\\)]* ;"),
+    State = str_trim(str_remove_all(State, ";")),
     series = str_trim(str_extract(series, "^[A-Za-z\\s]*"))
   ) %>%
-  select(date, series, state, value) %>%
+  select(Date, series, State, value) %>%
   pivot_wider(names_from="series", values_from="value") %>%
   rename(
     Takings = `Takings from accommodation`,
     Occupancy = `Room occupancy rate`
   ) %>%
   mutate(Takings = Takings / 1e6) %>%
-  filter(state != "Total (Area)") %>%
-  as_tsibble(index=date, key=state)
+  filter(State != "Total (Area)") %>%
+  as_tsibble(index=Date, key=State)
 
 cpi <- read_abs_local(filenames="640101.xlsx", path="data-raw/AUS_accommodation/") %>%
   filter(str_detect(series,"Australia"), data_type=="INDEX") %>%
-  mutate(date = yearquarter(date)) %>%
-  select(date, value) %>%
-  rename(cpi = value)
+  mutate(Date = yearquarter(date)) %>%
+  select(Date, value) %>%
+  rename(CPI = value)
 
 aus_accommodation <- aus_accommodation %>%
-  left_join(cpi, by="date")
+  left_join(cpi, by="Date")
 
-usethis::use_data(aus_accommodation, overwrite=FALSE)
+usethis::use_data(aus_accommodation, overwrite=TRUE)
