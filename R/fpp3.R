98ea7e5f00fdac8fb803454edf5e32b9901225f6
Rob Hyndman <robjhyndman@gmail.com>
2023-02-01 10:55:51 +1100

    Explicit importing of packages to load

--- R/fpp3.R
+++ R/fpp3.R
@@ -1,6 +1,17 @@
 # This package is based on the tidyverse package, hence the copyright
 # attribution to RStudio.
 
+#' @import dplyr
+#' @import fable
+#' @import fabletools
+#' @import feasts
+#' @import ggplot2
+#' @importFrom lubridate date
+#' @import tibble
+#' @import tidyr
+#' @import tsibble
+#' @import tsibbledata
+
 #' @keywords internal
 "_PACKAGE"
 
