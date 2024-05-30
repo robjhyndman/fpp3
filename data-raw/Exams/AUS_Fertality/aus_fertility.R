0eb79b4e19f806cb9e1911d7ce042160b4d7c98a
ShanikaLW <shanika.wickramasuriya@gmail.com>
2024-05-30 17:52:02 +1000

    fertility data added

---
+++ data-raw/Exams/AUS_Fertality/aus_fertility.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+library(tsibble)
+
+aus_fertility <- read_csv("data-raw/Exams/AUS_Fertality/ABS_FERTILITY_AGE_STATE_1.0.0_...A.csv")
+aus_fertility <- aus_fertility %>%
+  filter(`MEASURE: Measure` == "11: Fertility rate") %>%
+  select(-c(DATAFLOW, `MEASURE: Measure`, `FREQ: Frequency`, `UNIT_MEASURE: Unit of Measure`,
+            `OBS_STATUS: Observation Status`, `OBS_COMMENT: Observation Comment`)) %>%
+  rename(Age = `AGE: Age`,
+         Region = `REGION: Region`,
+         Year = `TIME_PERIOD: Time Period`,
+         Rate = OBS_VALUE) %>%
+  mutate(Age = stringr::str_split(Age, ": ", simplify = TRUE)[, 2],
+         Region = stringr::str_split(Region, ": ", simplify = TRUE)[, 2]) %>%
+  filter(!(Age %in% c("15-19", "20-24", "25-29", "30-34", "35-39",
+                      "40-44", "45-49", "All ages"))) %>%
+  select(Year, Region, Age, Rate) %>%
+  as_tsibble(index = Year, key = c(Age, Region))
+
+usethis::use_data(aus_fertility, overwrite=TRUE)
