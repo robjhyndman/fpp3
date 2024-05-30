eaf4be40c020d2ecc2e7d93b588b6add0748012e
ShanikaLW <shanika.wickramasuriya@gmail.com>
2024-05-30 22:09:15 +1000

    code updated

--- data-raw/Exams/NSW_Offence/offence.R
+++ data-raw/Exams/NSW_Offence/offence.R
@@ -2,15 +2,15 @@ library(tidyverse)
 library(tsibble)
 
 offences <- read_csv("data-raw/Exams/NSW_Offence/RCI_offencebymonth.csv")
-colnames(offences)[4:ncol(offences)] <- as.character(yearmonth("Jan 1995") + 0:347)
 
 offences <- offences %>%
-  pivot_longer(`1995 Jan`:`2023 Dec`, names_to = "Month", values_to = "Count") %>%
+  pivot_longer(-(1:3), names_to = "Month", values_to = "Count") %>%
   rename(Type = `Offence category`) %>%
   group_by(Type, Month) %>%
   summarise(Count = sum(Count)) %>%
   select(Month, Type, Count) %>%
-  mutate(Month = yearmonth(Month)) %>%
+  mutate(Month = lubridate::my(Month),
+         Month = yearmonth(Month)) %>%
   ungroup() %>%
   tsibble(index = Month, key = Type)
 
