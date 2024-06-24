90d40571a28d6ec9e514de715710650a0ce8e745
Rob J Hyndman <robjhyndman@gmail.com>
2024-06-24 17:35:40 +1000

    Updated aus_nom

--- data-raw/Exams/AUS_NOM/AUS_NOM.R
+++ data-raw/Exams/AUS_NOM/AUS_NOM.R
@@ -1,11 +1,13 @@
-library(tsibble)
-library(tidyverse)
-library(readxl)
+library(fpp3)
 
-aus_nom <- read_xlsx(path ="data-raw/Exams/AUS_NOM/migration_Updated.xlsx")[1:10] |>
+# Data downloaded from https://www.abs.gov.au/statistics/people/population/national-state-and-territory-population/
+# on 30 May 2024
+aus_nom <- readxl::read_xlsx(path = here::here("data-raw/Exams/AUS_NOM/migration_updated.xlsx"))[1:10] |>
   mutate(Quarter = yearquarter(Date)) |>
   pivot_longer(NSW:AUS, names_to = "State", values_to = "NOM") |>
   select(Quarter, State, NOM) |>
-  as_tsibble(index = Quarter, key = State)
+  as_tsibble(index = Quarter, key = State) |>
+  filter(State != "AUS")
 
+#aus_nom |> autoplot()
 usethis::use_data(aus_nom, overwrite=TRUE)
